5ba3328d6405e1d750237cae6eb3b25f6e72677c
Ch4zm of Hellmouth <ch4zm.of.hellmouth@gmail.com>
2021-12-07 21:06:29 -0800

    Automatic deploy of integration at 2021-12-08-05-06-29

--- theme/js/navbar.js
+++ theme/js/navbar.js
@@ -23,28 +23,77 @@
           throw "Could not find required property (season) in response from /mode API";
         }
 
+        // top level div element
+        var navbarSeasonDropdownDiv = document.createElement('div');
+        var navbarPostseasonDropdownDiv = document.createElement('div');
+
+        navbarSeasonDropdownDiv.classList.add('row');
+        navbarPostseasonDropdownDiv.classList.add('row');
+
+        navbarSeasonDropdownDiv.style.width = '400px';
+        navbarPostseasonDropdownDiv.style.width = '400px';
+
+        // ul element
+        var navbarSeasonDropdownUl = document.createElement('ul');
+        var navbarPostseasonDropdownUl = document.createElement('ul'); 
+
+        navbarSeasonDropdownUl.classList.add('list-unstyled');
+        navbarPostseasonDropdownUl.classList.add('list-unstyled');
+
+        navbarSeasonDropdownUl.classList.add('col-md-6');
+        navbarPostseasonDropdownUl.classList.add('col-md-6');
+
         var season0;
         for (season0 = 0; season0 <= modeApiResult.season; season0++) {
 
           var sp1 = parseInt(season0) + 1;
 
-          // Populate season drop-down
-          var sdropElem = document.createElement('a');
-          sdropElem.classList.add('dropdown-item');
-          //sdropElem.setAttribute('href', baseUIUrl + '/season.html?season=' + season0);
-          sdropElem.setAttribute('href', baseUIUrl + '/season.html?which_season=' + sp1);
-          sdropElem.innerHTML = 'Season ' + sp1;
-          navbarSeasonDropdown.appendChild(sdropElem);
+          var navbarSeasonDropdownLiElem = document.createElement('li');
+          var navbarPostseasonDropdownLiElem = document.createElement('li');
 
-          // Populate postseason drop-down
-          var pdropElem = document.createElement('a');
-          pdropElem.classList.add('dropdown-item');
-          //pdropElem.setAttribute('href', baseUIUrl + '/postseason.html?season=' + season0);
-          pdropElem.setAttribute('href', baseUIUrl + '/postseason.html?which_season=' + sp1);
-          pdropElem.innerHTML = 'Season ' + sp1;
-          navbarPostseasonDropdown.appendChild(pdropElem);
+          // Make Season link for season dropdown
+          var aElem;
+          aElem = document.createElement('a');
+          aElem.classList.add('dropdown-item');
+          aElem.setAttribute('href', baseUIUrl + '/season.html?which_season=' + sp1);
+          aElem.innerHTML = 'Season ' + sp1;
 
+          // Append li to ul
+          navbarSeasonDropdownLiElem.appendChild(aElem);
+          navbarSeasonDropdownUl.appendChild(navbarSeasonDropdownLiElem);
+
+          // Make Season link for postseason dropdown
+          aElem = document.createElement('a');
+          aElem.classList.add('dropdown-item');
+          aElem.setAttribute('href', baseUIUrl + '/season.html?which_season=' + sp1);
+          aElem.innerHTML = 'Season ' + sp1;
+
+          // Append li to ul
+          navbarPostseasonDropdownLiElem.appendChild(aElem);
+          navbarPostseasonDropdownUl.appendChild(navbarPostseasonDropdownLiElem);
+
+          if (sp1%12 == 0) {
+
+            // This season is the start of a new column,
+
+            // Tack on the old ul column:
+            navbarSeasonDropdownDiv.appendChild(navbarSeasonDropdownUl);
+            navbarPostseasonDropdownDiv.appendChild(navbarPostseasonDropdownUl);
+
+            // Make a new ul column:
+            var navbarSeasonDropdownUl = document.createElement('ul');
+            var navbarPostseasonDropdownUl = document.createElement('ul'); 
+
+            navbarSeasonDropdownUl.classList.add('list-unstyled');
+            navbarPostseasonDropdownUl.classList.add('list-unstyled');
+
+            navbarSeasonDropdownUl.classList.add('col-md-6');
+            navbarPostseasonDropdownUl.classList.add('col-md-6');
+          }
         }
+
+        navbarSeasonDropdown.appendChild(navbarSeasonDropdownDiv);
+        navbarPostseasonDropdown.appendChild(navbarPostseasonDropdownDiv);
 
       })
       .catch(err => {
